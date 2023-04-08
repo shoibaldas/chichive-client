971acd4932bfff6dbb567f799b5e9bf237eab1ac
Shoibal Das <your_email@example.com>
2023-04-08 21:33:39 +0600

    animation in each prouduct image integrated

--- src/pages/HomeProducts/HomeProduct.js
+++ src/pages/HomeProducts/HomeProduct.js
@@ -6,22 +6,30 @@ const HomeProduct = ({ product }) => {
 
   return (
     <div>
-        <div className="relative overflow-hidden transition duration-200 transform rounded shadow-lg hover:-translate-y-2 hover:shadow-2xl">
-          <img
-            className="object-cover w-full h-56 md:h-64 xl:h-80"
-            src={picture}
-            alt=""
-          />
-          <div className="absolute inset-0 px-6 py-4 transition-opacity duration-200 bg-black bg-opacity-75 opacity-0 hover:opacity-100">
-            <p className="mb-4 text-lg font-bold text-gray-100">{title}</p>
-            <p className="mb-4 text-lg font-bold text-gray-100">{price}</p>
-            <div className="flex justify-center">
-              <Link  to={`/booking/product/${product._id}`} className="mt-16 md:mt-28 bg-gray-600 hover:bg-black text-white py-2 px-4 rounded">
-                Book Now
-              </Link>
-            </div>
+      <div
+        data-aos="zoom-in"
+        data-aos-duration="1000"
+        data-aos-mirror="true"
+        className="relative overflow-hidden transition duration-200 transform rounded shadow-lg hover:-translate-y-2 hover:shadow-2xl"
+      >
+        <img
+          className="object-cover w-full h-56 md:h-64 xl:h-80"
+          src={picture}
+          alt=""
+        />
+        <div className="absolute inset-0 px-6 py-4 transition-opacity duration-200 bg-black bg-opacity-75 opacity-0 hover:opacity-100">
+          <p className="mb-4 text-lg font-bold text-gray-100">{title}</p>
+          <p className="mb-4 text-lg font-bold text-gray-100">{price}</p>
+          <div className="flex justify-center">
+            <Link
+              to={`/booking/product/${product._id}`}
+              className="mt-16 md:mt-28 bg-gray-600 hover:bg-black text-white py-2 px-4 rounded"
+            >
+              Book Now
+            </Link>
           </div>
         </div>
+      </div>
     </div>
   );
 };
